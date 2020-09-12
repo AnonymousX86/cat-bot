8941b25a45a4313fa3e6ba25b0fd6b9f46fa6e1c
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-09-12 13:55:50 +0200

    Initail commit
    Version 0.1.0-alpha.

---
+++ CatBot/__main__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from logging import basicConfig, INFO
+
+from discord import Game, Status
+from discord.ext.commands import Bot, ExtensionNotFound, ExtensionAlreadyLoaded, NoEntryPointError
+from nest_asyncio import apply as async_apply
+
+from settings import Settings
+
+
+def log(prefix: str, text: str):
+    """Just prettying print function"""
+    while len(prefix) < 6:
+        prefix += ' '
+    prefix = f'[{prefix}]'
+    print(f'{prefix} {text}')
+
+
+if __name__ == '__main__':
+    async_apply()
+    basicConfig(level=INFO)
+    bot = Bot(
+        command_prefix='ej ',
+        description='Private bot.',
+        owner_id=309270832683679745
+    )
+
+
+    @bot.event
+    async def on_ready():
+        log('Bot', 'Logged in as {0} (ID: {0.id})'.format(bot.user))
+        await bot.change_presence(status=Status.online, activity=Game(name='z kotami'))
+
+        for cog in [f'CatBot.cogs.{cog}' for cog in [
+            'basic'
+        ]]:
+            p = 'Cogs'
+            try:
+                bot.load_extension(cog)
+                log(p, f'Loaded: {cog}')
+            except ExtensionNotFound:
+                log(p, f'Not found: {cog}')
+            except ExtensionAlreadyLoaded:
+                log(p, f'Already loaded: {cog}')
+            except NoEntryPointError:
+                log(p, f'Extension "{cog}" do not have "setup()" function')
+            except Exception as e:
+                log(p, f'{e.__class__.__name__}: {e}')
+
+        log('Bot', 'Everything done!')
+
+
+    bot.run(Settings().bot_token)
---
+++ settings.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    @property
+    def bot_token(self) -> str:
+        return getenv("BOT_TOKEN")
+
+    @property
+    def bot_version(self) -> str:
+        return '0.1.0-alpha'
+
+    @property
+    def reddit_client_id(self):
+        return getenv('REDDIT_CLIENT_ID')
+
+    @property
+    def reddit_client_secret(self):
+        return getenv('REDDIT_CLIENT_SECRET')
+
+    @property
+    def reddit_user_agent(self):
+        return getenv('REDDIT_USER_AGENT').format(self.bot_version)
+
+    @property
+    def rapidapi_key(self):
+        return getenv('RAPIDAPI_KEY')
