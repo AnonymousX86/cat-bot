7a8bc05b0d39ddb1c094f2f4a7555bc677c35879
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-03-26 21:59:58 +0100

    Create music.py

---
+++ CatBot/cogs/music.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from discord.ext.commands import Cog, command, Context
+from wavelink import Client
+
+from CatBot.embeds.basic import custom_error_em, done_em
+
+
+class Music(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        if not hasattr(bot, 'wavelink'):
+            self.bot.wavelink = Client(bot=self.bot)
+
+        self.bot.loop.create_task(self.start_nodes())
+
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
+        await self.bot.wavelink.initiate_node(
+            host='127.0.0.1',
+            port='2333',
+            rest_uri='http://127.0.0.1:2333',
+            password='youshallnotpass',
+            identifier='TEST',
+            region='us_central'
+        )
+
+    @command(
+        name='connect'
+    )
+    async def connect_(self, ctx: Context):
+        try:
+            channel = ctx.author.voice.channel
+        except AttributeError:
+            return await ctx.message.reply(embed=custom_error_em('Dołącz się gdzieś'))
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        await player.connect(channel.id)
+
+    @command(
+        name='play'
+    )
+    async def play(self, ctx: Context, *, query: str = None):
+        if not query:
+            return await ctx.message.reply(embed=custom_error_em('Co mam włączyć?'))
+
+        tracks = await self.bot.wavelink.get_tracks(f'ytsearch:{query}')
+        if not tracks:
+            return await ctx.message.reply(embed=custom_error_em('Nic nie ma'))
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        if not player.is_connected:
+            await ctx.invoke(self.connect_)
+
+        await ctx.message.reply(embed=done_em(f'Dodano {str(tracks[0])} do kolejki'))
+        await player.play(tracks[0])
+
+
+def setup(bot):
+    bot.add_cog(Music(bot))
