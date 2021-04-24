7f0c5d9fbd25ce26596a03829a456e34e9bfd71c
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-04-24 22:09:10 +0200

    Update counters.py

--- CatBot/cogs/counters.py
+++ CatBot/cogs/counters.py
@@ -18,13 +18,14 @@ class Counters(Cog, name='Zliczanie'):
                     ' - Marta: narzeka,\n'
                     ' - Anon: informatykuje,\n'
                     ' - Kuba: nie ogarnia,\n'
-                    ' - Arras: przerywa zdanie,\n'
+                    ' - Arras: zmienia zdanie,\n'
                     ' - Agata: wkurza się,\n'
                     ' - Kira: wydaje podejrzane dźwięki,\n'
                     ' - Bezu: psuje metę.',
         aliases=['licz'],
         usage='[użytkownik] [opcja]',
-        help='Dostępne opcje:\n'
+        help='Bez podania użytkownika, podaje Twoje liczniki.\n'
+             'Dostępne opcje:\n'
              ' - pokazywanie: brak znaku,\n'
              ' - dodawanie: `+`, `plus`, `dodaj`.'
     )
@@ -37,9 +38,9 @@ class Counters(Cog, name='Zliczanie'):
             '220560592555999232': 'był niecierpliwy',  # Krystian
             '662713379416178690': 'był leniwy',  # Wojtek
             '560826194824790056': 'narzekała',  # Marta
-            '309270832683679745': 'informatykował',  # Kuba S.
-            '358274776806195210': 'nie ogarniał',  # Kuba K.
-            '247711780279681024': 'przerwał zdanie',  # Arras
+            '309270832683679745': 'informatykował',  # Anon
+            '358274776806195210': 'nie ogarniał',  # Kuba
+            '247711780279681024': 'zmienił zdanie',  # Arras
             '345242327289298946': 'wkurzyła się',  # Agata
             '389092372333723659': 'wydawała dziwne dźwięki',  # Kira
             '157854884668899329': 'psuł metę'  # Bezu
