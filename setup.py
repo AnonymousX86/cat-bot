e63eadb366dc8e74ef9b94a28efe6f9fda72867c
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2022-02-27 03:59:21 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cat-bot',
+    version='2021.7.2',
+    packages=['CatBot', 'CatBot.cogs', 'CatBot.utils', 'CatBot.embeds', 'CatBot.settings'],
+    url='https://github.com/AnonymousX86/cat-bot',
+    license='MIT',
+    author='Jakub Suchenek',
+    author_email='jakub.suchenek.25@gmail.com',
+    description='Private Discord bot.'
+)
